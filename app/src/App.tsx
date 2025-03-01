60a9af4d09b5b7aba966192b11a6b2a8a1133827
N2K6 <n2k6@MacBook-Air-N2K6.local>
2025-03-01 13:57:17 +0100

    add

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ProjectList from "./components/ProjectList";
+import { seedProjects } from "./data/seedProjects";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    seedProjects(); // Inicjalizacja przykładowych danych
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="text-center mt-4">
+        <h1 className="display-5 fw-bold text-primary">ManagMe - Projekty</h1>
+        <p className="lead">Zarządzaj swoimi projektami w prosty sposób</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <ProjectList />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- app/src/models/Project.ts
+++ app/src/models/Project.ts
@@ -3,4 +3,3 @@ export interface Project {
     name: string;
     description: string;
   }
-  
